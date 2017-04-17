4ed9b0149ea925077092b53e6d92fae3189d1754
Denis Bardadym <bardadymchik@gmail.com>
2017-04-17 12:53:03 +0300

    Make package.json work on windows and add pinkie promise for Promise

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-export default typeof fetch=='function' ? fetch : function(url, options) {
+import Promise from 'pinkie-promise';
+
+export default function unfetch(url, options) {
 	options = options || {};
 	return new Promise( (resolve, reject) => {
 		let request = new XMLHttpRequest();
